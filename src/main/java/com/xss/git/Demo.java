ebdd337ab24f98e0ce95d342f863b5b114f29705
eclipse_user <eclispe_user@xxx.com>
2020-07-19 15:08:13 +0800

    hot_fixed...

--- src/main/java/com/xss/git/Demo.java
+++ src/main/java/com/xss/git/Demo.java
@@ -3,5 +3,5 @@ public class Demo {
 	public static void main(String[] args) {
 		System.out.println("TestGit2,......");
 		System.out.println("testGit,......");
-
+		System.out.println("hot_fixed,......");
 }}
