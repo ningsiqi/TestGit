26403c39f283a3b6ebb756f8534181b420acedc9
eclipse_user <eclispe_user@xxx.com>
2020-07-19 14:41:26 +0800

    fixed2

--- src/main/java/com/xss/git/Demo.java
+++ src/main/java/com/xss/git/Demo.java
@@ -2,6 +2,7 @@ package com.xss.git;
 public class Demo {
 	public static void main(String[] args) {
 		System.out.println("testGit,......");
-		System.out.println("修复bugs......");
+		System.out.println("修复bugs1......");
+		System.out.println("修复bugs2......");
 	}
 }
